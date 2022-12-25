f32ac195f41c914f849f4890ef34c815996d1dfa
Abdullah_Sheikh <h.abdullahsheikh568@gmail.com>
2022-12-25 20:58:34 +0500

    assignment

---
+++ index.js
@@ -0,0 +1,52 @@
+#! /usr/bin/env node
+import chalk from "chalk";
+import inquirer from "inquirer";
+(async () => {
+    console.log(chalk.red("Welcome to Basic ATM"));
+    const usersInput = await inquirer.prompt([
+        {
+            name: "userId",
+            message: "Enter Your User ID",
+            type: "input"
+        },
+        {
+            name: "userPin",
+            message: "Enter Your Pin Number",
+            type: "password"
+        }
+    ]);
+    const userData = {
+        userId: usersInput.userId,
+        userPin: usersInput.userPin,
+        amount: Math.floor(Math.random() * 100000 + 1)
+    };
+    console.log(chalk.red("Hey", usersInput.userId, "How May I Help You..!"));
+    const selectoption = await inquirer.prompt([
+        {
+            name: "options",
+            message: "Select Any Options",
+            type: "list",
+            choices: ["Cash withdrawl", "Exit"],
+        },
+    ]);
+    if (selectoption.options === "Cash withdrawl") {
+        console.log(chalk.blueBright("Your Current Amount: ", userData.amount));
+        const enterAmount = await inquirer.prompt([
+            {
+                message: "Enter Your Amount",
+                name: "amount",
+                type: "number",
+                validate: (input) => {
+                    if (input > userData.amount) {
+                        return "insufficient Balance";
+                    }
+                    else {
+                        return true;
+                    }
+                }
+            }
+        ]);
+        userData.amount -= enterAmount.amount;
+        console.log(chalk.blueBright("Amount after withdrawl: ", userData.amount));
+    }
+})();
